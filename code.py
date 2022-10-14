e73c866e83391bbaba58ef2808e43718b5ff3a99
safimuhammad <safiwahid38@gmail.com>
2022-10-15 03:21:20 +0500

    pushing the envelope

--- code.py
+++ code.py
@@ -11,6 +11,7 @@ def vowelIdentifier(li=[],a=''):
     arr= list(test)
     state= False
     n=0
+    found=''
     index=[]
    
     vowels=['a','e','i','o','u']
@@ -21,14 +22,18 @@ def vowelIdentifier(li=[],a=''):
         if(a):
             if(a==i):
                 state= True
+                found=a
+                # found.insert(fList.index(a), a)
         if(fList):
             for j in fList:
                 if(j == i):
                     index.append(fList.index(j))
                     # print(index)
                     n+=1
+                    
+
                    
-    return state,n
+    return state,n,found,index
     
 # specimen 1
 def vowelMover():
@@ -109,6 +114,7 @@ def test3():
     a=[]
     temp=''
     i=0
+    lastVowel=''
     
     maxi=len(arr)
     while i < maxi:
@@ -120,34 +126,46 @@ def test3():
             for j in range(i+1,len(arr)):
                 if(vowelIdentifier(arr,arr[j])[0]==True):
                     totalVowels=vowelIdentifier(arr,arr[j])[1]
+                    lastVowel=vowelIdentifier(arr,arr[j])[2]
+
                     arr.insert(j, arr[i])
                     arr.pop(i)
                     n+=1
                     vowelDisplaced=n
-                    temp=j
+                    temp=arr[j]
                     print(''.join(arr))
                     # print(temp,a[0])
                     break
-      
+                if(temp == lastVowel):
+                    print('working')
+                    for k in test:
+                        if(vowelIdentifier(arr,k)[0]==True):
+                            arr.insert(arr.index(k),temp)
+                            arr.pop(arr.index(temp))
+                            print(''.join(arr))
+                            break
+
+                
 
 
                 
     
-        if(totalVowels+1==vowelDisplaced):
+        if(totalVowels==vowelDisplaced):
+            print('working2')
             # print(arr[temp],vowelDisplaced)
             maxi=a[0]
             i=0  
-            print(''.join(arr))
+            print(''.join(arr),temp)
             # i=0
             break
-    
+   
             
-# test3()
+test3()
     
-myList = ['a','b','c']
-for i in range(len(myList)):
-    myTuple = myList[i],myList[(i+1)%len(myList)]
-    print(myTuple)
+# myList = ['a','b','c']
+# for i in range(len(myList)):
+#     myTuple = myList[i],myList[(i+1)%len(myList)]
+#     print(myTuple)
        
     
 
